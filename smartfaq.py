8a61a090db4428858731a87643e590d197a0a970
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-08-10 16:45:07 +0800

    Update smartfaq.py

--- smartfaq.py
+++ smartfaq.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import openai
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
